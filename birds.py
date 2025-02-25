25c2f54f4ede1da91100dd7dbf38aa14f3a68966
NoraxMine <NoraxMine@tutamail.com>
2025-02-25 13:27:42 +0300

    pz1(v3)

--- birds.py
+++ birds.py
@@ -1,12 +1,13 @@
 class Birds:
     
-    def __init__ (self,name,fly_ability,size):
+    def __init__ (self,name,fly_ability,size,flying_range):
         self.fly_ability = fly_ability
         self.name = name
         self.size = size
-    
+        self.flying_range = flying_range
+
     def info(self):
-        print (f"fly_ability:{self.fly_ability},name:{self.name},size:{self.size}")
+        print (f"fly_ability:{self.fly_ability},name:{self.name},size:{self.size},flying_range:{self.flying_range}")
     
     def sounding(self):
         print(f"True")
@@ -17,17 +18,19 @@ class Birds:
     def slepping(self):
         print(f"No")
 
+    def flying_range(self):
+        if self.fly_ability == True:
+            self.flying_range = self.size * 21
+        else:
+            self.fly_ability == False
+        print (f"flying_range:{self.flying_range}")
+
     def __del__ (self):
         print ("del done", self.name)
-
+     
 
 def bis():
-        B = Birds("Guise" ,True, "Big")
+        B = Birds("Guise" ,True, "Big", True)
         B.info()
+bis()
 
-
-def main():
-        bis()
-
-if __name__ == '__main__':
-        main()
