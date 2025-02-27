81382e14f9c24fcbc2157e292f441c7d21e21c23
NoraxMine <NoraxMine@tutamail.com>
2025-02-27 13:19:37 +0300

    call

---
+++ tests.py
@@ -0,0 +1,29 @@
+class Calk:
+
+    def __init__ (self,a,b):
+        self.a = a
+        self.b = b
+
+    def info(self):
+        print (f"{self.a + self.b}")
+
+    def info3(self):
+        print (f"{self.a - self.b}")
+
+    def info1(self):
+        print (f"{self.a * self.b}")
+
+    def info2(self):
+        print (f"{self.a / self.b}")
+
+    def __del__ (self):
+        print ("")
+     
+
+def bis():
+        B = Calk(a = int(input()),b = int(input()))
+        B.info()
+        B.info1()
+        B.info2()
+        B.info3()
+bis()
